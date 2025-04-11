0850e8cf70c70638685c6fc7261f825aaf08e022
kmcfad5135 <kendall.mcfadden@howardcc.edu>
2025-04-10 22:20:09 -0400

    MyApp

--- app/src/main/java/com/kodeco/myapp/MainActivity.kt
+++ app/src/main/java/com/kodeco/myapp/MainActivity.kt
@@ -40,12 +40,19 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        fontSize = 20.sp,
+        //Change font size
+        fontSize = 30.sp,
+        //set color of font
         color = Color.Blue,
         modifier = modifier
+            //set padding around text
             .padding(20.dp)
+            //set background
             .background(Color(0xFFADD8E6))
+            //make borders
             .border(3.dp, Color.Black)
+            //change size of text container
+            .size(130.dp)
             .padding(10.dp)
 
 
